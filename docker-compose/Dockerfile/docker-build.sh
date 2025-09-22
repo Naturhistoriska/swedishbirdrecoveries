97c1821c6f0acb1cb3f20be9c061825d62e875ad
shahmanash <manash.shah@gmail.com>
2025-09-22 16:41:30 +0200

    removed redundant docker related files

--- docker-compose/Dockerfile/docker-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source .env
-echo $VERSION
-docker build -t inkimar/shiny:${VERSION} .
--- docker-compose/bin/refresh-swedishbirdrecoveries.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker exec shiny \
-Rscript /usr/local/lib/R/site-library/swedishbirdrecoveries/exec/update_data.R
-
-docker restart shiny
