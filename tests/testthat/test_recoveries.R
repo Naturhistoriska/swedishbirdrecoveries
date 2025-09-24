cdae19bdcd94f35863d50ef5eff51cb01fad463a
shahmanash <manash.shah@gmail.com>
2025-09-24 18:49:17 +0200

    skip some tests in ci

--- tests/testthat/test_recoveries.R
+++ tests/testthat/test_recoveries.R
@@ -9,12 +9,14 @@ test_that("datasets are not empty", {
 })
 
 test_that("scrape returns data - checklists", {
+	skip_on_ci()
   expect_gt(nrow(scrape_checklist_falsterbo()), 0)
   expect_gt(nrow(scrape_checklist_norway()), 0)
   expect_gt(nrow(scrape_checklist_ottenby()), 0)
 })
 
 test_that("scrape returns data - recoveries", {
+	skip_on_ci()
   expect_gt(nrow(scrape_recoveries_falsterbo()), 0)
   #expect_gt(nrow(scrape_recoveries_norway()), 0)
   expect_gt(nrow(scrape_recoveries_ottenby()), 0)
