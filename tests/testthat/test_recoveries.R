6c4b356d8f8d4ec9ce764f2530ad0d70006781d1
shahmanash <manash.shah@gmail.com>
2025-10-01 15:16:26 +0200

    remove deprecated functions from test

--- tests/testthat/test_recoveries.R
+++ tests/testthat/test_recoveries.R
@@ -7,17 +7,3 @@ test_that("datasets are not empty", {
   expect_gt(nrow(birdrecoveries_eng), 0)
 	expect_gt(nrow(birdrecoveries_swe), 0)
 })
-
-test_that("scrape returns data - checklists", {
-	skip_on_ci()
-  expect_gt(nrow(scrape_checklist_falsterbo()), 0)
-  expect_gt(nrow(scrape_checklist_norway()), 0)
-  expect_gt(nrow(scrape_checklist_ottenby()), 0)
-})
-
-test_that("scrape returns data - recoveries", {
-	skip_on_ci()
-  expect_gt(nrow(scrape_recoveries_falsterbo()), 0)
-  #expect_gt(nrow(scrape_recoveries_norway()), 0)
-  expect_gt(nrow(scrape_recoveries_ottenby()), 0)
-})
